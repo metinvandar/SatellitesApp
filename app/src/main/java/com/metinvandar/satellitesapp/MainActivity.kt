0e3b34b3ca0f92d43f5b83c7e215d0b14a1c90ae
metin <metinvandar@gmail.com>
2024-10-19 20:37:59 +0300

    initial commit

---
+++ app/src/main/java/com/metinvandar/satellitesapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.metinvandar.satellitesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/metinvandar/satellitesapp/application/SatellitesApplication.kt
@@ -0,0 +1,8 @@
+package com.metinvandar.satellitesapp.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SatellitesApplication: Application() {
+}
